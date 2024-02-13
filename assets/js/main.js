e28e218eb6385c007a02207294541de69cbc9b3d
Rob Shea <robshea@gmail.com>
2024-02-12 23:56:32 -0500

    update table based on selectors

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,72 @@
+// f-numbers
+const f_numbers = [1.4, 2, 2.8, 4, 5.6, 8, 11, 16, 22, 32];
+// wavelengths
+const wavelengths = [350, 400, 470, 550, 590, 665, 720, 780, 830, 930, 1000]
+
 function updatePitch() {
+
   // Get the selected value from the "camera" select field
-  const pitchValue = document.querySelector('#camera').value;
+  const pitch_micron = document.querySelector('#camera').value;
   // Update the value of the "pitch" input field
-  document.getElementById("pitch").value = pitchValue;
+  document.getElementById("pitch").value = pitch_micron;
+
+  let cell_id = '';
+  // loop over f-numbers
+  let f = 0;
+  while (f < f_numbers.length) {
+
+    // loop over wavelengths
+    let w = 0;
+    let cell_class = "";
+    while (w < wavelengths.length) {
+
+      cell_id = f_numbers[f] + "-"+wavelengths[w];
+      ratio = airyPitchRatio(f_numbers[f], wavelengths[w], pitch_micron);
+      document.getElementById(cell_id).innerHTML = ratio;
+
+      if (ratio <= 3) {
+        cell_class = "good";
+      } else if (ratio >= 4.5) {
+        cell_class = "bad";
+      } else {
+        cell_class = "maybe";
+      }
+
+      var element = document.getElementById(cell_id);
+      element.classList.remove("good", "maybe", "bad");
+      element.classList.add(cell_class);
+      w++;
+    }
+    f++;
+  }
+}
+
+function airyPitchRatio(f_number, wavelength, pitch_micron) {
+  let pitch_nm = pitch_micron * 1000;
+  let ratio = 2 * 1.22 * f_number * wavelength / pitch_nm;
+  let rounded = Math.round(ratio * 100) / 100;
+  return rounded;
+}
+
+function updateFilter(filterWavelengths) {
+  var wavelengthsList = filterWavelengths.value;
+  let w = 0;
+  let cell_class = "";
+  while (w < wavelengths.length) {
+    var elements = document.querySelectorAll(".w"+wavelengths[w]);
+
+    for (var i = 0; i < elements.length; i++) {
+      if (wavelengthsList.includes(wavelengths[w], 0)) {
+        // show column
+        elements[i].classList.remove("hide", "show");
+        elements[i].classList.add("show");
+      } else {
+        // hide column
+        elements[i].classList.remove("hide", "show");
+        elements[i].classList.add("hide");
+      }     
+    }
+    w++;
+
+  }
 }
--- index.md
+++ index.md
@@ -13,8 +13,17 @@ layout: splash
   <option value="{{ camera.pitch }}">{{ camera.make }} {{ camera.model }}</option>
   {% endfor %}
 </select>
+
 <label for="pitch">Pixel Pitch (μm)</label>
 <input type="text" id="pitch" name="pitch" disabled>
+
+<label for="filter">Filter</label>
+<select name="filter" id="filter" onchange="updateFilter(this)">
+  {% for filter in site.data.filters.filters %}
+  <option value="{{ filter.wavelengths }}">{{ filter.filter }}</option>
+  {% endfor %}
+</select>
+
 </form>
 
 <table id="diff-table">
@@ -26,18 +35,26 @@ layout: splash
 <thead>
   <tr>
     <th></th>
+    <th colspan="2" id="uv">Ultraviolet</th>
+    <th colspan="5" id="visible">Visible Light</th>
+    <th colspan="4" id="ir">Infrared</th>
+  </tr>
+  <tr>
+    <th></th>
     {% for wavelength in site.data.wavelengths.wavelengths %}
-      <th>{{ wavelength.wavelength }}</th>
+      <th id="w{{ wavelength.wavelength }}">{{ wavelength.wavelength }}</th>
     {% endfor %}
   </tr>
 </thead>
 <tbody>
   {% for f-number in site.data.f-numbers.f-number %}
-    <tr><td>{{ f-number.f-stop }}</td>
+    <tr><td>ƒ/{{ f-number.f-stop }}</td>
     {% for wavelength in site.data.wavelengths.wavelengths %}
-      <td id="{{ f-number.f-stop }}-{{ wavelength.wavelength }}">{{ wavelength.wavelength }}</td>
+      <td id="{{ f-number.f-stop }}-{{ wavelength.wavelength }}" class="w{{ wavelength.wavelength }}"></td>
     {% endfor %}
     </tr>
   {% endfor %}
 </tbody>
 </table>
+
+### How to Read this Table
